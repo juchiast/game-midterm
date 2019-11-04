4f78a2ff8e5b6d5aa79eaaaa08f6e01ff00bd465
Duy Do <juchiast@gmail.com>
2019-11-04 09:01:31 +0700

    Init

---
+++ Assets/VehicleControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleControl : MonoBehaviour
+{
+    private GameObject FrontWheel, RearWheel;
+
+    private Rigidbody2D RearWheelBody, FrontWheelBody;
+    
+    private Camera MainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FrontWheel = GameObject.Find("FrontWheel");
+        RearWheel = GameObject.Find("RearWheel");
+        RearWheelBody = RearWheel.GetComponent<Rigidbody2D>();
+        FrontWheelBody = FrontWheel.GetComponent<Rigidbody2D>();
+        MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RearWheelBody.AddTorque(30);
+            FrontWheelBody.AddTorque(30);    
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            RearWheelBody.AddTorque(-30);
+            FrontWheelBody.AddTorque(-30);
+        }
+        
+        DoCamera();
+    }
+    
+    private void DoCamera() {
+        Vector3 pos = MainCamera.transform.position;
+        pos.x = transform.position.x;
+        MainCamera.transform.position = pos;
+    }
+}
