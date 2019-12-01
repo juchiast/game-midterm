3ce5bd1a137e17e9dbb8cb2d58b1306933ead1eb
Duy Do <juchiast@gmail.com>
2019-12-01 17:24:36 +0700

    wut

---
+++ Assets/CarSelector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarSelector : MonoBehaviour
+{
+    public static int SelectedCar;
+
+    public static readonly String[] CarNames = {"Blue", "Red", "Truck"};
+
+    private GameObject[] CarImages;
+
+    private GameObject[] CarButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedCar = 0;
+
+        CarImages = new[]
+        {
+            GameObject.Find("ImageBlue"),
+            GameObject.Find("ImageRed"),
+            GameObject.Find("ImageTruck"),
+        };
+
+        CarButtons = new[]
+        {
+            GameObject.Find("BtnBlue"),
+            GameObject.Find("BtnRed"),
+            GameObject.Find("BtnTruck"),
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject image in CarImages)
+        {
+            image.SetActive(false);
+        }
+        
+        CarImages[SelectedCar].SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CarSelector");
+    }
+}
---
+++ Assets/MapSelector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapSelector : MonoBehaviour
+{
+    public static String SelectedMap;
+    public static String SelectedCar;
+
+    private GameObject MapCheck;
+    private GameObject BtnWinter;
+    private GameObject BtnNight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedMap = "NightMap";
+        SelectedCar = "Blue";
+        
+        MapCheck = GameObject.Find("MapCheck");
+        BtnWinter = GameObject.Find("BtnWinter");
+        BtnNight = GameObject.Find("BtnNight");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SelectedMap == "NightMap")
+        {
+            MapCheck.transform.position = BtnNight.transform.position;
+        }
+        if (SelectedMap == "WinterMap")
+        {
+            MapCheck.transform.position = BtnWinter.transform.position;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CarSelector");
+    }
+
+    public void SelectNightMap()
+    {
+        SelectedMap = "NightMap";
+    }
+
+    public void SelectWinterMap()
+    {
+        SelectedMap = "WinterMap";
+    }
+}
